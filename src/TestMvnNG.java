17b649dfed6aa30c3edadd3c66954f3fcb7ff718
Rubel <bittechcorp@gmail.com>
2013-11-02 22:57:21 -0400

    new class

---
+++ src/TestMvnNG.java
@@ -0,0 +1,32 @@
+
+public class TestMvnNG 
+{
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.firefox.FirefoxDriver;
+	import org.testng.annotations.AfterTest;
+	import org.testng.annotations.BeforeTest;
+	import org.testng.annotations.Test;
+
+
+	public class MavenTestNG 
+	{
+	FirefoxDriver driver=new FirefoxDriver();
+
+	@BeforeTest
+	public void start()
+	{
+		driver.get("http://www.facebook.com");
+	}
+	@AfterTest
+	public void end()
+	{
+		driver.quit();
+	}
+	@Test
+	public void login()
+	{
+		driver.findElement(By.id("email")).sendKeys("fgdgfg@yahoo");
+		}
+	}
+
+}
